9bec4be7b628c6734954530cf1bdfa72eafdb9ed
den1zzzkaw0w <markgoretin4321@gmail.com>
2026-05-17 02:45:05 +0300

    add sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,12 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://doja-china-blog.vercel.app",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://doja-china-blog.vercel.app/articles",
+      lastModified: new Date(),
+    },
+  ];
+}
